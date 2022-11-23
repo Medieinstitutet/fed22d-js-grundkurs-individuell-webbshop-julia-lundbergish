23a545ce937a84f4e4a4f973aa7b765230791f75
julia-lundbergish <julialundberg@hotmail.se>
2022-11-23 21:52:43 +0100

    JS progress men allt annat trasigt

--- script.js
+++ script.js
@@ -1,88 +1,172 @@
-//* Objekt 
+//* Objekt
 
 const donuts = [
   {
-    name: 'Red velvet',
+    name: "Red velvet",
     price: 22,
     rating: 4,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
   },
   {
-    name: 'Caramel Crisp',
+    name: "Caramel Crisp",
     price: 16,
     rating: 4,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
   },
   {
-    name: 'La La Latte',
+    name: "La La Latte",
     price: 14,
     rating: 5,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
   },
   {
-    name: 'Tutti Frutti',
+    name: "Tutti Frutti",
     price: 14,
     rating: 3,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
   },
   {
-    name: 'Oreo',
+    name: "Oreo",
     price: 16,
     rating: 5,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
   },
   {
-    name: 'Choklad',
+    name: "Choklad",
     price: 12,
     rating: 4,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
   },
   {
-    name: 'Viol',
+    name: "Viol",
     price: 16,
     rating: 3,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
   },
   {
-    name: 'Lime key pie',
+    name: "Lime key pie",
     price: 20,
     rating: 4,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
   },
   {
-    name: 'Cheesecake',
+    name: "Cheesecake",
     price: 16,
     rating: 4,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
   },
   {
-    name: 'Glazed',
+    name: "Glazed",
     price: 12,
     rating: 5,
     amount: 0,
+    sum: 0,
+    imgFile: "images/special-donut.png",
+  },
+];
+
+//* Munkcontainer
+
+const donutContainer = document.querySelector("#donutContainer");
+
+function renderDonuts() {
+  donutContainer.innerHTML = "";
+
+  for (let i = 0; i < donuts.length; i++) {
+    donutContainer.innerHTML += `
+    <section id="donutContainer">
+      <div class="donuts">
+        <article class="donutGrid">
+        <img src="${donuts[i].imgFile}" alt="${donuts[i].name}" height="240">
+          <div class="donutContent">
+          <h3 class="donutName">${donuts[i].name}</h2>
+          <div class="donutDetails">
+          <div class="donutPrice">
+          <div class="donutAmount">
+          <span class="price">${donuts[i].price}</span> kr
+          </div>
+            <span class="amount">${donuts[i].amount}</span> st
+            <span class="sum">${donuts[i].sum} kr</span>
+            <button class="minus" data-id="${i}">-</button>
+            <button class="plus" data-id="${i}">+</button>  
+          </div>
+          </div>
+          </div>
+          </div>
+        </article>
+    </section>
+    `;
   }
-]
 
-const donutContainer = document.querySelector('#donutContainer');
+  document.querySelectorAll("button.plus").forEach((btn) => {
+    btn.addEventListener("click", updateDonutAmount);
+  });
 
-for (let i = 0; i < donuts.length; i++) {
-  donutContainer.innerHTML += `
-  <article class="donutGrid">
-  <div class="donutContent">
-  <h3 class="donutName">${donuts[i].name}</h2>
-  <span class="price">${donuts[i].price} kr</span>
-  <button class="plus" data-id="${i}"></button>
-  </article>
-  `;
+  document.querySelectorAll("button.minus").forEach((btn) => {
+    btn.addEventListener("click", updateDonutAmountMinus);
+  });
+
+  //* Nedanstående funkar inte? 
+
+  // const amount = donuts.reduce((previousValue, donut) => {
+  //   return donut.amount + previousValue;
+  // }, 0);
+
+  // const sum = donuts.reduce((previousValue, donut) => {
+  //   return donut.price + previousValue;
+  // }, 0);
+
+  // document.querySelector("#cartSum").innerHTML = amount;
+  // document.querySelector("#cartSum").innerHTML = sum;
+
 }
+
+//* Öka antal munkar vid klick av plusknapp
 
 function updateDonutAmount(e) {
-  console.log(e.currentTarget.dataset.id);
+  const amountEl = e.currentTarget.parentElement.querySelector(".amount");
+
+  let amount = Number(amountEl.innerText);
+  amountEl.innerHTML = amount + 1;
+
+  updateDonutSum(e.currentTarget.parentElement);
 }
 
-document.querySelectorAll('button.plus').forEach((btn) => {
-  btn.addEventListener('click', updateDonutAmount);
-})
+//* Minska antal munkar vid klick av minusknapp
+
+function updateDonutAmountMinus(e) {
+  const amountEl = e.currentTarget.parentElement.querySelector(".amount");
+
+  let amount = Number(amountEl.innerText);
+
+  if (amount - 1 < 0) {
+    return;
+  }
+
+  amountEl.innerHTML = amount - 1;
+
+  updateDonutSum(e.currentTarget.parentElement);
+}
+
+renderDonuts();
 
 //* Kommentar jag inte har kommit på vad den ska heta än
 
@@ -99,32 +183,6 @@ for (let i = 0; i < decreaseButtons.length; i++) {
   increaseButtons[i].addEventListener("click", increaseCount);
 }
 
-//* Öka antal munkar vid klick av plusknapp
-
-function increaseCount(e) {
-  const amountEl = e.currentTarget.parentElement.querySelector(".amount");
-
-  let amount = Number(amountEl.innerText);
-  amountEl.innerHTML = amount + 1;
-
-  updateDonutSum(e.currentTarget.parentElement);
-}
-
-//* Minska antal munkar vid klick av minusknapp
-
-function decreaseCount(e) {
-  const amountEl = e.currentTarget.parentElement.querySelector(".amount");
-
-  let amount = Number(amountEl.innerText);
-
-  if (amount - 1 < 0) {
-    return;
-  }
-
-  amountEl.innerHTML = amount - 1;
-
-  updateDonutSum(e.currentTarget.parentElement);
-}
 
 //* Uppdatera kostnad av munk vid klick av plus och minus
 
